59ff3047e91b031e5398d50c5cdff89eb90052fe
edbert19 <ed79bert@gmail.com>
2025-04-02 14:49:40 +0700

    Commit 1 Edbert: Menyelesaikan api 'dungeons'

--- src/api/routes.js
+++ src/api/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const games = require('./components/games/games-route');
 const staff = require('./components/staff/staff-route');
 const places = require('./components/places/places-route');
+const dungeons = require('./components/dungeons/dungeons-route');
 
 module.exports = () => {
   const app = express.Router();
@@ -10,6 +11,7 @@ module.exports = () => {
   games(app);
   staff(app);
   places(app);
+  dungeons(app);
   
   return app;
 };
