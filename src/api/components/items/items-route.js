2dbd38bcd840f65ec4c33e4f479e3cbab4266081
wirawahyu <wirawahyu225@gmail.com>
2025-04-07 09:12:21 +0700

    Comit 1 Wira: Menyelesaikan api 'items'

---
+++ src/api/components/items/items-route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const itemsController = require('./items-controller');
+
+const route = express.Router();
+
+module.exports = (app) => {
+  app.use('/items', route);
+
+  route.get('/name/:name', itemsController.getItemsByName);
+  route.get('/:id', itemsController.getItemsById);
+  route.get('/', itemsController.getItems);
+  route.post('/', itemsController.createItems);
+};
