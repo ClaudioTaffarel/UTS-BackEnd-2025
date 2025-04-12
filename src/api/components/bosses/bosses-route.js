04a0e1e7b25cbac053569183ec8fe3bfb07195c0
tegarning01 <tegaruntar@gmail.com>
2025-04-12 18:15:20 +0700

    commit 1 tegar : component monster & dungeon

---
+++ src/api/components/bosses/bosses-route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bossesController = require('./bosses-controller');
+
+const route = express.Router();
+
+module.exports = (app) => {
+  app.use('/bosses', route);
+
+  route.get('/name/:name', bossesController.getBossByName);
+  route.get('/:id', bossesController.getBossById);
+  route.get('/', bossesController.getBosses);
+  route.post('/', bossesController.createBosses);
+};
---
+++ src/api/components/monsters/monsters-services.js
@@ -0,0 +1,30 @@
+const monstersRepository = require('./monsters-repository');
+
+async function getMonsters(limit, page) {
+  return monstersRepository.getMonsters(limit, page);
+}
+
+async function getMonsterById(id) {
+  return monstersRepository.getMonstersById(id);
+}
+
+async function getMonsterByName(name) {
+  return monstersRepository.getMonsterByName(name);
+}
+
+async function monsterNameExists(name) {
+  const monster = await monstersRepository.getMonsterByName(name);
+  return !!monster;
+}
+
+async function createMonsters(name, description, appearance) {
+  return monstersRepository.createMonsters(name, description, appearance);
+}
+
+module.exports = {
+    getMonsters,
+    getMonsterById,
+    getMonsterByName,
+    monsterNameExists,
+    createMonsters,
+};
